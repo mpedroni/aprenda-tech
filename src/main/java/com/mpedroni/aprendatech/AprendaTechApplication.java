6006e1b69b31c984caaa477ccf8f826782735b9b
mpedroni <mpedroni77@gmail.com>
2024-03-21 11:55:05 -0300

    first commit :tada:

---
+++ src/main/java/com/mpedroni/aprendatech/AprendaTechApplication.java
@@ -0,0 +1,13 @@
+package com.mpedroni.aprendatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendaTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AprendaTechApplication.class, args);
+    }
+
+}
