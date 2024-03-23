d81704527e4a808216d0b8fa71f16612ce8e8141
mpedroni <mpedroni77@gmail.com>
2024-03-23 18:03:34 -0300

    test: improve user controller unit test readability
    Created helper functions to remove duplicated code

--- src/test/java/com/mpedroni/aprendatech/api/UserControllerUnitTest.java
+++ src/test/java/com/mpedroni/aprendatech/api/UserControllerUnitTest.java
@@ -4,6 +4,7 @@ import com.mpedroni.aprendatech.ControllerTest;
 import com.mpedroni.aprendatech.domain.users.usecases.CreateUserUseCase;
 import com.mpedroni.aprendatech.infra.users.api.UserController;
 import com.mpedroni.aprendatech.infra.users.persistence.UserJpaEntity;
+import io.restassured.module.mockmvc.response.MockMvcResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -18,26 +19,21 @@ public class UserControllerUnitTest {
 
     @Test
     void createsAStudent() {
-        var request = UserRequestBuilder.builder().post();
+        var body = new CreateUserRequestBody();
 
-        var name = request.name();
-        var username = request.username();
-        var email = request.email();
-        var password = request.password();
-        var role = request.role();
+        var name = body.name();
+        var username = body.username();
+        var email = body.email();
+        var password = body.password();
+        var role = body.role();
 
         when(createUserUseCase.execute(name, username, email, password, role))
             .thenReturn(
-                UserJpaEntity.from(1L, request.name(), request.username(), request.email(), request.password(), request.role()
+                UserJpaEntity.from(1L, body.name(), body.username(), body.email(), body.password(), body.role()
             )
         );
 
-        given()
-            .auth().none()
-            .contentType("application/json")
-            .body(request.build())
-        .when()
-            .post("/users")
+        givenCreateUserRequest(body.build())
         .then()
             .statusCode(201)
             .header("Location", matchesPattern("/users/[\\d+]"));
@@ -45,14 +41,9 @@ public class UserControllerUnitTest {
 
     @Test
     void throwsAnErrorWhenRequiredFieldsAreBlank() {
-        var request = UserRequestBuilder.builder().blank().post();
+        var body = new CreateUserRequestBody().blank().build();
 
-        given()
-            .auth().none()
-            .contentType("application/json")
-            .body(request.build())
-        .when()
-            .post("/users")
+        givenCreateUserRequest(body)
         .then()
             .statusCode(400)
             .body("errors", hasItems(
@@ -67,14 +58,9 @@ public class UserControllerUnitTest {
     @Test
     void throwsAnErrorWhenUsernameHasInvalidLength() {
         var aUsernameWithMoreThan20Characters = "a".repeat(21);
-        var request = UserRequestBuilder.builder().withUsername(aUsernameWithMoreThan20Characters).post();
+        var body = new CreateUserRequestBody().withUsername(aUsernameWithMoreThan20Characters).build();
 
-        given()
-            .auth().none()
-            .contentType("application/json")
-            .body(request.build())
-        .when()
-            .post("/users")
+        givenCreateUserRequest(body)
         .then()
             .statusCode(400)
             .body("errors", hasItem("Username must have at most 20 characters."));
@@ -83,14 +69,9 @@ public class UserControllerUnitTest {
     @Test
     void throwsAnErrorWhenUsernameHasInvalidCharacters() {
         var aUsernameWithInvalidCharacters = "J0hn Do3@";
-        var request = UserRequestBuilder.builder().withUsername(aUsernameWithInvalidCharacters).post();
+        var body = new CreateUserRequestBody().withUsername(aUsernameWithInvalidCharacters).build();
 
-        given()
-            .auth().none()
-            .contentType("application/json")
-            .body(request.build())
-        .when()
-            .post("/users")
+        givenCreateUserRequest(body)
         .then()
             .statusCode(400)
             .body("errors", hasItem("Username must have only lower case characters, without numbers or blank spaces."));
@@ -99,14 +80,9 @@ public class UserControllerUnitTest {
     @Test
     void throwsAnErrorWhenEmailIsInvalid() {
         var anInvalidEmail = "john@doe";
-        var request = UserRequestBuilder.builder().withEmail(anInvalidEmail).post();
+        var body = new CreateUserRequestBody().withEmail(anInvalidEmail).build();
 
-        given()
-            .auth().none()
-            .contentType("application/json")
-            .body(request.build())
-        .when()
-            .post("/users")
+        givenCreateUserRequest(body)
         .then()
             .statusCode(400)
             .body("errors", hasItem("Email must be a well-formed email address."));
@@ -115,12 +91,12 @@ public class UserControllerUnitTest {
     @Test
     void throwsAnErrorWhenRoleIsInvalid() {
         var anInvalidRole = "INVALID";
-        var request = UserRequestBuilder.builder().withRole(anInvalidRole).post();
+        var body = new CreateUserRequestBody().withRole(anInvalidRole).build();
 
         given()
             .auth().none()
             .contentType("application/json")
-            .body(request.build())
+            .body(body)
         .when()
             .post("/users")
         .then()
@@ -128,20 +104,25 @@ public class UserControllerUnitTest {
             .body("errors", hasItem("Role must be STUDENT, INSTRUCTOR or ADMIN."));
     }
 
-    private static class UserRequestBuilder {
+    static MockMvcResponse givenCreateUserRequest(String body) {
+        return given()
+            .auth().none()
+            .contentType("application/json")
+            .body(body)
+        .when()
+            .post("/users");
+    }
+
+    private static class CreateUserRequestBody {
         private String name = "John Doe";
         private String username = "johndoe";
         private String email = "john@doe.com";
         private String password = "password@123";
         private String role = "STUDENT";
 
-        private String result = "";
+        public CreateUserRequestBody() {}
 
-        static UserRequestBuilder builder() {
-            return new UserRequestBuilder();
-        }
-
-        public UserRequestBuilder blank() {
+        public CreateUserRequestBody blank() {
             name = "";
             username = "";
             email = "";
@@ -150,17 +131,17 @@ public class UserControllerUnitTest {
             return this;
         }
 
-        public UserRequestBuilder withUsername(String username) {
+        public CreateUserRequestBody withUsername(String username) {
             this.username = username;
             return this;
         }
 
-        public UserRequestBuilder withEmail(String email) {
+        public CreateUserRequestBody withEmail(String email) {
             this.email = email;
             return this;
         }
 
-        public UserRequestBuilder withRole(String role) {
+        public CreateUserRequestBody withRole(String role) {
             this.role = role;
             return this;
         }
@@ -186,11 +167,7 @@ public class UserControllerUnitTest {
         }
 
         public String build() {
-            return result;
-        }
-
-        public UserRequestBuilder post() {
-            result =  """
+            return """
             {
                 "name": "%s",
                 "username": "%s",
@@ -199,8 +176,6 @@ public class UserControllerUnitTest {
                 "role": "%s"
             }
             """.formatted(name, username, email, password, role);
-
-            return this;
         }
     }
 }
